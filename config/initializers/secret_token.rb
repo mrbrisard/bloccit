076fce3e8cae52bfa7c28ce657eedc3ebdcc3eb1
AJ Brisard <abrisard@gmail.com>
2014-10-18 19:07:09 +0000

    Still trying to fix this figaro config

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Bloccit::Application.config.secret_key_base = '281e8c1372cfd48fbbcb2c912b89a02f2737ac028f2e2d503c21d08b3c77ec6ded92094d8d83aacda5da61e6e51604789f021f108f4f3f0fce12244fac00d28e'
+Bloccit::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
