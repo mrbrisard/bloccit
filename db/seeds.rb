2b6bcd930602fa2f19642981c183289af532772b
AJ Brisard <abrisard@gmail.com>
2014-10-19 01:20:03 +0000

    Completing associating-with-users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,43 @@
 require 'faker'
 
- # Create Posts
- 50.times do
-   Post.create!(
-     title:  Faker::Lorem.sentence,
-     body:   Faker::Lorem.paragraph
-   )
- end
- posts = Post.all
- 
- # Create Comments
- 100.times do
-   Comment.create!(
-     post: posts.sample,
-     body: Faker::Lorem.paragraph
-   )
- end
+# Create Users
+5.times do
+  user = User.new(
+    name:     Faker::Name.name,
+    email:    Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save!
+end
+users = User.all
+
+# Create Posts
+50.times do
+  Post.create!(
+    user:   users.sample,
+    title:  Faker::Lorem.sentence,
+    body:   Faker::Lorem.paragraph
+  )
+end
+posts = Post.all
+
+# Create Comments
+100.times do
+  Comment.create!(
+    # user: users.sample,   # we have not yet associated Users with Comments
+    post: posts.sample,
+    body: Faker::Lorem.paragraph
+  )
+end
+
+ User.first.update_attributes!(
+  email: 'abrisard@gmail.com',
+  password: 'reallytesting',
+)
  
  puts "Seed finished"
+ puts "#{User.count} users created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
 
